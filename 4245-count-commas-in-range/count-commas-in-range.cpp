f36a9c91344fddb9d5aa86db7f33b3d1d2e09b56
Saurav Bhandari <skp72140@gmail.com>
2026-09-09 21:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.72%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -4,9 +4,9 @@ public:
         
         int count = 0;
 
-        for(int i = 1000; i <= n; i++) {
+        for(int i = 1000; i <= n; i++)
+        
         count++;
-        }
 
         return count;
     }
