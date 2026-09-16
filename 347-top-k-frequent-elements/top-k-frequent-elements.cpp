0735c6c6008488c7081ef12ddd061562cced5066
Saurav Bhandari <skp72140@gmail.com>
2026-09-17 03:09:34 +0530

    Time: 2 ms (58.79%) | Memory: 18.2 MB (33.84%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> m;
+        
+        for (int x : nums) m[x]++;
+
+        priority_queue<pair<int,int>> p;
+
+        for (auto x : m) {
+            p.push({x.second,x.first});
+        }
+
+        vector<int>ans;
+
+        while(ans.size() < k) {
+        ans.push_back(p.top().second);
+        p.pop();
+        }
+
+    return ans;
+
+    }
+};
