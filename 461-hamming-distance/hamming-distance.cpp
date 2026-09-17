7d96bdb43d59ac286d8beacf557d279ffcfedb54
Saurav Bhandari <skp72140@gmail.com>
2026-09-17 23:09:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (4.55%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string helperbts(int num) {
+        string ans;
+        while (num>0) {
+            if (num % 2 == 0)
+                ans += '0';
+            else
+                ans += '1';
+            num /= 2;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    int hammingDistance(int x, int y) {
+        // convertion into the bits
+        string str1 = helperbts(x);
+        string str2 = helperbts(y);
+
+        while (str1.size() > str2.size()) {
+            str2.insert(str2.begin(),'0');
+        }
+
+        while(str1.size() < str2.size()) {
+            str1.insert(str1.begin(),'0');
+        }
+
+        int i = 0,j=str1.size(),ans=0;
+        while (i<str1.size()) {
+        if(str1[i]!=str2[i]) ans++; 
+        i++;
+        }
+        return ans;
+    }
+};
