07affc0be8efd481bf7cf4b4fda24be2265671e8
Saurav Bhandari <skp72140@gmail.com>
2026-09-23 19:42:02 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int ans = 0;
+        int dp[3];
         if (nums.size() == 1) return nums.front();
         else if (nums.size() == 2)
            return max(nums[0], nums[1]);
         else {
-            int dp[3];
             dp[0]=nums.front();
             dp[1]=max(nums[0], nums[1]);
             for (int i = 2; i < nums.size(); i++) {
@@ -14,8 +13,7 @@ public:
                 dp[0]=dp[1];// 11
                 dp[1]=dp[2];//
             }
-           ans = dp[2];
         }
-return ans;
+return dp[2];
     }
 };
